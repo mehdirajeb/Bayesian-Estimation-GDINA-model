e069e112cd4d2294cbf8680aca06e33784081666
Mehdi Rajeb <97915215+mehdirajeb@users.noreply.github.com>
2024-01-01 18:19:27 -0600

    Add files via upload

---
+++ 002. HGDINA-Worked.R
@@ -0,0 +1,132 @@
+library(GDINA)
+library(rjags)
+library(R2jags)
+
+####################################################################
+#Data form GDINA package
+###################################################################
+
+Y <- Rep11_score[,-1]
+Q <- Q
+all.patterns <- GDINA::attributepattern(ncol(Q))
+
+
+###################################################################################
+#Hierarchical GDINA model information
+# We have all the same setup. Used 3 attributes.
+# n= individual
+# i = item
+# j = attribute
+# alpha(n,k) = individual's attribute profile
+###################################################################################
+
+##############################################
+#JAGS model: HGDINA
+##############################################
+
+jags.hgdina <- function() {
+  for (n in 1:N) {
+    for (i in 1:I) { # for k= 3, we may estimate maximum 8 parameters for GDINA model. 
+      
+      eta1[n, i] <- lamda1[i]* alpha[n,1] + lamda2[i]* alpha[n,2]+ lamda3[i]* alpha[n,3]
+      
+      eta2[n,i] <- lamda12[i]* alpha[n,1]* alpha[n,2] + lamda13[i]* alpha[n,1]* alpha[n,3] + lamda23[i]* alpha[n,2]* alpha[n,3]
+      
+      eta3[n,i] <- lamda123[i]* alpha[n,1]* alpha[n,2]* alpha[n,3]
+      
+      logit(p[n, i]) <- lamda0[i]+ eta1[n,i] + eta2[n,i]+ eta3[n,i] ## used logit link
+      
+      Y[n, i] ~ dbern(p[n, i])
+    }}
+    
+  for (n in 1:N){
+    for (k in 1: K){
+      logit(prob.a[n,k])<- xi[k]* theta[n] - lamda[k]
+      alpha[n,k] ~ dbern(prob.a[n,k])
+    }
+    theta[n] ~ dnorm(0,1)
+  }
+  
+  for(k in 1: K){
+    lamda[k]~ dnorm(0, .25)
+    xi[k] ~ dnorm(0, .25) %_% T(0,)
+  }
+    
+  
+  for (i in 1:I) {
+    lamda0[i] ~ dnorm(-1.096, 0.25)
+    xlamda1[i] ~ dnorm(0, 0.25)%_% T(0,)
+    xlamda2[i] ~ dnorm(0, 0.25)%_% T(0,) 
+    xlamda3[i] ~ dnorm(0, 0.25)%_% T(0,) 
+    xlamda12[i] ~ dnorm(0, 0.25)
+    xlamda13[i] ~ dnorm(0, 0.25)
+    xlamda23[i] ~ dnorm(0, 0.25)
+    xlamda123[i] ~ dnorm(0, 0.25)
+    lamda1[i] <- xlamda1[i] * Q[i, 1]
+    lamda2[i] <- xlamda2[i] * Q[i, 2]
+    lamda3[i] <- xlamda3[i] * Q[i, 3]
+    lamda12[i] <- xlamda12[i] * Q[i, 1] * Q[i, 2]
+    lamda13[i] <- xlamda13[i] * Q[i, 1] * Q[i, 3]
+    lamda23[i] <- xlamda23[i] * Q[i, 2] * Q[i, 3]
+    lamda123[i] <- xlamda123[i] * Q[i, 1] * Q[i, 2] * Q[i, 3]}
+  
+  
+  ## the posterior predictive model checking## I need to understand this part more thoroughly
+  for (n in 1:N) {
+    for (i in 1:I) {
+      teststat[n, i] <- pow(Y[n, i] - p[n, i], 2)/(p[n, i] * (1 -
+                                                                p[n, i]))
+      Y_rep[n, i] ~ dbern(p[n, i])
+      teststat_rep[n, i] <- pow(Y_rep[n, i] - p[n, i], 2)/(p[n, i] *
+                                                             (1 - p[n, i]))
+    }
+  }
+  teststatsum <- sum(teststat[1:N, 1:I])
+  teststatsum_rep <- sum(teststat_rep[1:N, 1:I])
+  ppp <- step(teststatsum_rep - teststatsum)
+}
+######################################################################################
+#####################################################################################
+## USEd 100 items for practice purpose
+N <- nrow(Y)  
+I <- nrow(Q)
+K <- ncol(Q)
+jags.data <- list("N", "I", "K", "Y", "Q")
+#################################################################
+# removed unused data
+################################################################
+jags.parameters <- c("lamda0","lamda1","lamda2", "lamda3", "lamda12","lamda13","lamda23","lamda123","theta","lamda","ppp")  
+jags.inits <- NULL #Initial values are not specified.
+time1 <- as.POSIXlt(Sys.time())
+
+#####################################################################################
+#RJAGs code
+####################################################################################
+
+hgdina <- jags(data = jags.data, inits = jags.inits, parameters.to.save = jags.parameters,
+            model.file = jags.hgdina, n.chains = 2, n.iter = 20000, n.burnin = 10000,
+            n.thin = 1, DIC = TRUE)
+time2 <- as.POSIXlt(Sys.time())
+use.time <- difftime(time2, time1, units = "secs")
+
+######################################################################################
+#Outputs: Theta is included
+######################################################################################
+hgdina1 <- hgdina$BUGSoutput
+hgdina1$summary
+
+write.csv(hgdina1$summary, "Rep11_HDINA-20k.csv")
+
+print(hgdina1)
+
+E.pattern <- cbind(sim1$median$c)
+sim1$mean$ppp
+E.itempar <- cbind(sim1$mean$lamda0, sim1$sd$lamda0, sim1$mean$lamda1,sim1$sd$lamda1,
+                   sim1$mean$lamda2, sim1$sd$lamda2, sim1$mean$lamda3,sim1$sd$lamda3,
+                   sim1$mean$lamda12, sim1$sd$lamda12, sim1$mean$lamda13,sim1$sd$lamda13,
+                   sim1$mean$lamda23, sim1$sd$lamda23, sim1$mean$lamda123,sim1$sd$lamda123,
+                   sim1$mean$theta,sim1$sd$theta)
+colnames(E.itempar) <- c("lamda0", "SE(lamda0)", "lamda1", "SE(lamda1)", "lamda2", "SE(lamda2)", 
+                         "lamda3", "SE(lamda3)","lamda12", "SE(lamda12)", "lamda13", "SE(lamda13)",
+                         "lamda23", "SE(lamda23)","lamda123", "SE(lamda123)", "Theta", "SE(Theta)")
+E.itempar
